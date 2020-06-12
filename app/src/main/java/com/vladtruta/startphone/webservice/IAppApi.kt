3536ff1a2682830eb9db060e4927172b0e0ef127
Vlad Truta <vlad.truta@tapptitude.com>
2020-06-13 00:25:41 +0300

    Fixed API GET

--- app/src/main/java/com/vladtruta/startphone/webservice/IAppApi.kt
+++ app/src/main/java/com/vladtruta/startphone/webservice/IAppApi.kt
@@ -3,11 +3,11 @@ package com.vladtruta.startphone.webservice
 import com.vladtruta.startphone.model.responses.ApiTutorialResponse
 import com.vladtruta.startphone.model.responses.BaseResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface IAppApi {
 
-    @GET("tutorials/{packageName}")
-    suspend fun getTutorialsForPackageName(@Path("packageName") packageName: String): BaseResponse<List<ApiTutorialResponse>>
+    @GET("tutorials")
+    suspend fun getTutorialsForPackageName(@Query("packageName") packageName: String): BaseResponse<List<ApiTutorialResponse>>
 
 }
